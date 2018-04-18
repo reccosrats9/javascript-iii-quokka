8703f1f646e4f0bbe98dba9687a50335ef820ecb
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-04-18 13:54:52 -0600

    all portions complete

--- 7 classes/classesPractice1.js
+++ 7 classes/classesPractice1.js
@@ -3,3 +3,26 @@
 // Add a method to update the best time
 // Add a method to update the farthest difficulty
 // Add a method to return the best time and farthest difficulty on an object
+
+class Ninja{
+    constructor(name, bestTime, farthest){
+        this.name= name;
+        this.bestTime= bestTime;
+        this.farthest= farthest
+    }
+    updateTime(time){
+        if (this.bestTime> bestTune>newTime){
+            this.bestTime=newTime
+        }
+    }
+    updateDifficulty(){
+        this.farthest++; //not quite what the question is asking, but it's what we did in class
+    }
+    display(){
+        return `The amazing ninja ${this.name} completed the course in ${this.bestTime} seconds, with a difficulty of ${this.farthest}`
+    }
+}
+let Shannon= new Ninja('Shan', 9, 222);
+Shannon
+Shannon.updateDifficulty();
+Shannon
